2d245bc401862b57518dfb70d9d1a0e57739b7d8
Hoang Hai <20130166@st.hcmuaf.edu.vn>
2022-05-26 23:24:23 +0700

    da sua loi

--- src/view/HomePage.java
+++ src/view/HomePage.java
@@ -46,14 +46,13 @@ public class HomePage extends JFrame {
 	/**
 	 * Initialize the contents of the frame.
 	 */
-private void initialize() {
+	private void initialize() {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 1200, 700);
 		frame.setResizable(false);
 		frame.getContentPane().setBackground(State.background);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-
 		JButton btnCenter = new JButton("Home Page");
 		btnCenter.setBackground(new Color(135, 206, 250));
 		btnCenter.setFont(new Font("Serif", Font.BOLD, 30));
@@ -64,14 +63,12 @@ private void initialize() {
 		});
 		btnCenter.setBounds(510, 65, 200, 70);
 		frame.getContentPane().add(btnCenter);
-
 //----------------------------------------------------
 		JPanel pnlTop = new JPanel();
 		pnlTop.setBackground(State.background);
 		pnlTop.setBounds(10, 10, 1165, 90);
 		frame.getContentPane().add(pnlTop);
 		pnlTop.setLayout(null);
-
 		JButton btnSetting = new JButton("Setting");
 		btnSetting.setFont(new Font("Serif", Font.BOLD, 25));
 		btnSetting.setIcon(new ImageIcon(HomePage.class.getResource("/libs/iconSetting.png")));
@@ -81,29 +78,26 @@ private void initialize() {
 		btnSetting.setFocusable(false);
 		btnSetting.setBorder(null);
 		pnlTop.add(btnSetting);
-
 		JLabel lblLogo = new JLabel("");
 		lblLogo.setIcon(new ImageIcon(HomePage.class.getResource("/libs/logo-small.png")));
 		lblLogo.setBounds(23, -22, 100, 112);
 		pnlTop.add(lblLogo);
-
 		JLabel lblGroup17 = new JLabel("GROUP 17");
 		lblGroup17.setForeground(Color.WHITE);
 		lblGroup17.setFont(new Font("Serif", Font.BOLD, 18));
 		lblGroup17.setBounds(106, 24, 137, 48);
 		pnlTop.add(lblGroup17);
 //		----------------------------------
-
 		JPanel pnlLeft = new JPanel();
 		pnlLeft.setBackground(new Color(135, 206, 250));
 		pnlLeft.setBounds(10, 100, 200, 550);
 		frame.getContentPane().add(pnlLeft);
 		pnlLeft.setLayout(null);
-
 		JPanel pnlInfor = new JPanel();
 		pnlInfor.setBackground(new Color(175, 238, 238));
 		pnlInfor.setLayout(null);
 		pnlInfor.setBorder(new LineBorder(new Color(0, 0, 0), 4));
+		pnlInfor.setBounds(10, 40, 180, 100);
 		pnlInfor.setBounds(10, 20, 180, 100);
 		pnlLeft.add(pnlInfor);
 
@@ -111,26 +105,23 @@ private void initialize() {
 		lblGetName.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblGetName.setBounds(75, 35, 95, 13);
 		pnlInfor.add(lblGetName);
-
 		JLabel lblGetRank = new JLabel(".............................................................................");
 		lblGetRank.setFont(new Font("Tahoma", Font.BOLD, 14));
 		lblGetRank.setBounds(75, 67, 95, 13);
 		pnlInfor.add(lblGetRank);
-
 		JLabel lblName = new JLabel("Name:");
 		lblName.setBounds(9, 29, 61, 22);
 		pnlInfor.add(lblName);
 		lblName.setFont(new Font("Tahoma", Font.BOLD, 18));
-
 		JLabel lblRank = new JLabel("Rank:");
 		lblRank.setBounds(10, 58, 61, 22);
 		pnlInfor.add(lblRank);
 		lblRank.setFont(new Font("Tahoma", Font.BOLD, 18));
-
 		JPanel pnlContructorAvailableRooms = new JPanel();
 		pnlContructorAvailableRooms.setBackground(new Color(175, 238, 238));
 		pnlContructorAvailableRooms.setLayout(null);
 		pnlContructorAvailableRooms.setBorder(new LineBorder(new Color(0, 0, 0), 4));
+		pnlContructorAvailableRooms.setBounds(30, 300, 140, 50);
 		pnlContructorAvailableRooms.setBounds(30, 260, 140, 50);
 		pnlLeft.add(pnlContructorAvailableRooms);
 
@@ -138,11 +129,11 @@ private void initialize() {
 		lblAvailableRooms.setBounds(12, 12, 123, 22);
 		pnlContructorAvailableRooms.add(lblAvailableRooms);
 		lblAvailableRooms.setFont(new Font("Tahoma", Font.BOLD, 14));
-
 		JPanel pnlAvailableRooms = new JPanel();
 		pnlAvailableRooms.setBackground(new Color(175, 238, 238));
 		pnlAvailableRooms.setLayout(null);
 		pnlAvailableRooms.setBorder(new LineBorder(new Color(0, 0, 0), 4));
+		pnlAvailableRooms.setBounds(10, 180, 180, 150);
 		pnlAvailableRooms.setBounds(10, 140, 180, 150);
 		pnlLeft.add(pnlAvailableRooms);
 
@@ -152,11 +143,11 @@ private void initialize() {
 		lblGetAvailableRooms.setBounds(25, 45, 135, 55);
 		pnlAvailableRooms.add(lblGetAvailableRooms);
 		lblGetAvailableRooms.setFont(new Font("Tahoma", Font.BOLD, 25));
-
 		JPanel pnlContructoReservedRooms = new JPanel();
 		pnlContructoReservedRooms.setBackground(new Color(175, 238, 238));
 		pnlContructoReservedRooms.setLayout(null);
 		pnlContructoReservedRooms.setBorder(new LineBorder(new Color(0, 0, 0), 4));
+		pnlContructoReservedRooms.setBounds(30, 480, 140, 50);
 		pnlContructoReservedRooms.setBounds(30, 440, 140, 50);
 		pnlLeft.add(pnlContructoReservedRooms);
 
@@ -167,20 +158,20 @@ private void initialize() {
 
 		JPanel pnlReservedRooms = new JPanel();
 		pnlReservedRooms.setBackground(new Color(175, 238, 238));
+		pnlReservedRooms.setBounds(10, 360, 180, 150);
 		pnlReservedRooms.setBounds(10, 320, 180, 150);
 		pnlLeft.add(pnlReservedRooms);
 		pnlReservedRooms.setLayout(null);
 		pnlReservedRooms.setBorder(new LineBorder(new Color(0, 0, 0), 4));
-
 		JLabel lblGetReservedRooms = new JLabel("1/100");
 		lblGetReservedRooms.setHorizontalAlignment(SwingConstants.CENTER);
 		lblGetReservedRooms.setForeground(Color.BLACK);
 		lblGetReservedRooms.setFont(new Font("Tahoma", Font.BOLD, 25));
 		lblGetReservedRooms.setBounds(25, 45, 135, 55);
 		pnlReservedRooms.add(lblGetReservedRooms);
-		
 
-		
+
+
 		JButton btnLogOut = new JButton("Log Out");
 		btnLogOut.setForeground(Color.WHITE);
 		btnLogOut.setBounds(51, 505, 89, 23);
@@ -188,26 +179,23 @@ private void initialize() {
 		btnLogOut.setBackground(State.red_button);
 		pnlLeft.add(btnLogOut);
 
-		
+
 //		---------------------------------		
 		JPanel pnlRight = new JPanel();
 		pnlRight.setBounds(215, 100, 960, 550);
 		frame.getContentPane().add(pnlRight);
 		pnlRight.setLayout(null);
-
 		JPanel pnlCheckIn = new JPanel();
 		pnlCheckIn.setLayout(null);
 		pnlCheckIn.setBorder(new LineBorder(new Color(0, 0, 0), 4));
 		pnlCheckIn.setBackground(new Color(135, 206, 250));
 		pnlCheckIn.setBounds(100, 60, 300, 200);
 		pnlRight.add(pnlCheckIn);
-
 		JLabel lblCheckIn = new JLabel("Check In");
 		lblCheckIn.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCheckIn.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 45));
 		lblCheckIn.setBounds(10, 145, 280, 40);
 		pnlCheckIn.add(lblCheckIn);
-
 		JButton btnCheckIn = new JButton("");
 		btnCheckIn.setIcon(new ImageIcon(HomePage.class.getResource("/libs/log-in.png")));
 		btnCheckIn.setHorizontalAlignment(SwingConstants.CENTER);
@@ -216,20 +204,17 @@ private void initialize() {
 		btnCheckIn.setBackground(new Color(135, 206, 250));
 		btnCheckIn.setBounds(10, 10, 280, 135);
 		pnlCheckIn.add(btnCheckIn);
-
 		JPanel pnlRooms = new JPanel();
 		pnlRooms.setBackground(new Color(135, 206, 250));
 		pnlRooms.setBorder(new LineBorder(new Color(0, 0, 0), 4));
 		pnlRooms.setBounds(540, 60, 300, 200);
 		pnlRight.add(pnlRooms);
 		pnlRooms.setLayout(null);
-
 		JLabel lblRooms = new JLabel("Rooms");
 		lblRooms.setHorizontalAlignment(SwingConstants.CENTER);
 		lblRooms.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 45));
 		lblRooms.setBounds(10, 145, 280, 40);
 		pnlRooms.add(lblRooms);
-
 		JButton btnRooms = new JButton("");
 		btnRooms.setIcon(new ImageIcon(HomePage.class.getResource("/libs/room.png")));
 		btnRooms.setHorizontalAlignment(SwingConstants.CENTER);
@@ -238,20 +223,17 @@ private void initialize() {
 		btnRooms.setFocusable(false);
 		btnRooms.setBorder(null);
 		pnlRooms.add(btnRooms);
-
 		JPanel pnlCheckOut = new JPanel();
 		pnlCheckOut.setBackground(new Color(135, 206, 250));
 		pnlCheckOut.setBorder(new LineBorder(new Color(0, 0, 0), 4));
 		pnlCheckOut.setBounds(100, 320, 300, 200);
 		pnlRight.add(pnlCheckOut);
 		pnlCheckOut.setLayout(null);
-
 		JLabel lblCheckOut = new JLabel("Check  Out");
 		lblCheckOut.setHorizontalAlignment(SwingConstants.CENTER);
 		lblCheckOut.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 45));
 		lblCheckOut.setBounds(10, 145, 280, 40);
 		pnlCheckOut.add(lblCheckOut);
-
 		JButton btnCheckOut = new JButton("");
 		btnCheckOut.setIcon(new ImageIcon(HomePage.class.getResource("/libs/log-out.png")));
 		btnCheckOut.setHorizontalAlignment(SwingConstants.CENTER);
@@ -260,20 +242,17 @@ private void initialize() {
 		btnCheckOut.setFocusable(false);
 		btnCheckOut.setBorder(null);
 		pnlCheckOut.add(btnCheckOut);
-
 		JPanel pnlGuest = new JPanel();
 		pnlGuest.setLayout(null);
 		pnlGuest.setBackground(new Color(135, 206, 250));
 		pnlGuest.setBorder(new LineBorder(new Color(0, 0, 0), 4));
 		pnlGuest.setBounds(540, 320, 300, 200);
 		pnlRight.add(pnlGuest);
-
 		JLabel lblGuest = new JLabel("Guest");
 		lblGuest.setHorizontalAlignment(SwingConstants.CENTER);
 		lblGuest.setFont(new Font("Serif", Font.BOLD | Font.ITALIC, 45));
 		lblGuest.setBounds(10, 145, 280, 40);
 		pnlGuest.add(lblGuest);
-
 		JButton btnGuest = new JButton("");
 		btnGuest.setIcon(new ImageIcon(HomePage.class.getResource("/libs/guest.png")));
 		btnGuest.setHorizontalAlignment(SwingConstants.CENTER);
@@ -282,6 +261,5 @@ private void initialize() {
 		btnGuest.setBackground(new Color(135, 206, 250));
 		btnGuest.setBounds(10, 10, 280, 135);
 		pnlGuest.add(btnGuest);
-
 	}
 }
