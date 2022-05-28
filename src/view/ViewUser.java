e419e4357d3c72554bdbd69200dc0f83799a130a
binhchau0303 <binhchau0303@gmail.com>
2022-05-29 02:21:00 +0700

    sua loi roomType va room capacity

--- src/view/ViewUser.java
+++ src/view/ViewUser.java
@@ -16,7 +16,7 @@ import javax.swing.table.DefaultTableModel;
 
 public class ViewUser extends JFrame {
 
-	private JTable table;
+	private JTable tableViewUser;
 
 	/**
 	 * Launch the application.
@@ -43,44 +43,43 @@ public class ViewUser extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		getContentPane().setLayout(null);
 
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-		panel.setBounds(10, 10, 564, 90);
-		panel.setBackground(new Color(32, 83, 117));
-		getContentPane().add(panel);
+		JPanel north_panel = new JPanel();
+		north_panel.setLayout(null);
+		north_panel.setBounds(10, 10, 564, 90);
+		north_panel.setBackground(new Color(32, 83, 117));
+		getContentPane().add(north_panel);
 
-		JButton btnNewButton_1 = new JButton("<Back");
-		btnNewButton_1.setForeground(Color.WHITE);
-	
+		JButton back_button = new JButton("<Back");
+		back_button.setForeground(Color.WHITE);
 
-		btnNewButton_1.setBounds(25, 10, 81, 27);
-		btnNewButton_1.setBackground(new Color(32, 83, 117));
+		back_button.setBounds(25, 10, 81, 27);
+		back_button.setBackground(new Color(32, 83, 117));
 
-		btnNewButton_1.setFocusable(false);
-		panel.add(btnNewButton_1);
+		back_button.setFocusable(false);
+		north_panel.add(back_button);
 
-		JLabel lblNewLabel_6 = new JLabel("");
-		lblNewLabel_6.setIcon(new ImageIcon("libs/logo-small.png"));
-		lblNewLabel_6.setBounds(173, -28, 128, 128);
-		panel.add(lblNewLabel_6);
+		JLabel iconLogo_label = new JLabel("");
+		iconLogo_label.setIcon(new ImageIcon("libs/logo-small.png"));
+		iconLogo_label.setBounds(173, -28, 128, 128);
+		north_panel.add(iconLogo_label);
 
-		JLabel lblNewLabel_7 = new JLabel("GROUP 17");
-		lblNewLabel_7.setForeground(Color.WHITE);
-		lblNewLabel_7.setFont(new Font("Serif", Font.BOLD, 18));
-		lblNewLabel_7.setBounds(252, 38, 98, 27);
-		panel.add(lblNewLabel_7);
+		JLabel group17_label = new JLabel("GROUP 17");
+		group17_label.setForeground(Color.WHITE);
+		group17_label.setFont(new Font("Serif", Font.BOLD, 18));
+		group17_label.setBounds(252, 38, 98, 27);
+		north_panel.add(group17_label);
 
-		JPanel panel_1 = new JPanel();
-		panel_1.setLayout(null);
-		panel_1.setBackground(Color.LIGHT_GRAY);
-		panel_1.setBounds(10, 111, 564, 439);
-		getContentPane().add(panel_1);
+		JPanel table_panel = new JPanel();
+		table_panel.setLayout(null);
+		table_panel.setBackground(Color.LIGHT_GRAY);
+		table_panel.setBounds(10, 111, 564, 439);
+		getContentPane().add(table_panel);
 
-		table = new JTable();
-		table.setBackground(Color.WHITE);
-		table.setFont(new Font("Time New Roman", Font.BOLD, 11));
-		table.setBounds(0, 0, 564, 439);
-		table.setModel(new DefaultTableModel(
+		tableViewUser = new JTable();
+		tableViewUser.setBackground(Color.WHITE);
+		tableViewUser.setFont(new Font("Time New Roman", Font.BOLD, 11));
+		tableViewUser.setBounds(0, 0, 564, 439);
+		tableViewUser.setModel(new DefaultTableModel(
 				new Object[][] { { "User Name", "Password", "Admin Status" }, { null, null, null },
 						{ null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
 						{ null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
@@ -90,20 +89,15 @@ public class ViewUser extends JFrame {
 						{ null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
 						{ null, null, null }, { null, null, null }, { null, null, null }, },
 				new String[] { "User Name", "Password", "Admin Status" }));
-		panel_1.add(table);
-		
-		
+		table_panel.add(tableViewUser);
 
-		
-		
-		
-		//addActionListener	
-		btnNewButton_1.addActionListener(new ActionListener() {
+		// addActionListener
+		back_button.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		
-		btnNewButton_1.addActionListener(new ActionListener() {
+
+		back_button.addActionListener(new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -117,14 +111,11 @@ public class ViewUser extends JFrame {
 				refreshFrame.setSize(600, 600);
 			}
 		});
-		
-		lblNewLabel_7.addMouseListener(State.retureHomePage(lblNewLabel_7, this));
 
-		lblNewLabel_6.addMouseListener(State.retureHomePage(lblNewLabel_6, this));
-		
-		
-		
-		
+		group17_label.addMouseListener(State.retureHomePage(group17_label, this));
+
+		iconLogo_label.addMouseListener(State.retureHomePage(iconLogo_label, this));
+
 	}
 
 }
